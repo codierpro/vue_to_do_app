1b4ee37dfd041dfc32195011c77cc8c9498fda2a
codierpro <46135038+codierpro@users.noreply.github.com>
2020-02-25 13:03:22 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ToDo/>
+  </div>
+</template>
+
+<script>
+import ToDo from './components/ToDo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ToDo
+  }
+}
+</script>
+
+<style>
+
+</style>
